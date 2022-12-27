93df24e9010772ecf5196efcc358b1ecbaeed698
ellieshami <eli.shami@mail.huij.ac.il>
2022-12-27 15:52:42 +0200

    opens an empty window of default size

--- src/pepse/PepseGameManager.java
+++ src/pepse/PepseGameManager.java
@@ -1,20 +1,26 @@
 package pepse;
 
 import danogl.GameManager;
+import danogl.util.Vector2;
 
 public class PepseGameManager extends GameManager {
 
+    static Vector2 windowSize = new Vector2(700, 500);
+
     PepseGameManager(){
-        super();
+        super("", windowSize);
     }
 
+    /*
     @Override
-    public void initializeGame(danogl.gui.ImageReader imageReader, danogl.gui.SoundReader
+        public void initializeGame(danogl.gui.ImageReader imageReader, danogl.gui.SoundReader
             soundReader,
                          danogl.gui.UserInputListener inputListener,
                         danogl.gui.WindowController windowController){
 
     }
+     */
+
     public static void main(String[] args){
         new PepseGameManager().run();
     }
