c3a9a3010d8e3c79eb43c7bb9f35687720d776d6
ellieshami <eli.shami@mail.huij.ac.il>
2022-12-31 14:23:16 +0200

    begining to create sunHalo

--- src/pepse/world/dayNight/SunHalo.java
+++ src/pepse/world/dayNight/SunHalo.java
@@ -1,2 +1,21 @@
-package pepse.world.dayNight;public class SunHalo {
+package pepse.world.dayNight;
+
+import danogl.GameObject;
+import danogl.collisions.GameObjectCollection;
+import danogl.gui.rendering.OvalRenderable;
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
+
+import java.awt.*;
+
+public class SunHalo {
+
+    public static GameObject create(GameObjectCollection gameObjects, int layer,
+                                    GameObject sun, Color color) {
+        Vector2 location = Vector2.ZERO;
+        Renderable haloRenderable = new OvalRenderable(color);
+        GameObject halo = new GameObject(location, sun.getDimensions().add(new Vector2(1,1)), haloRenderable);
+        return null;
+    }
+
 }
