b3ea62694ec8ce09a59fea915e87a4927388f1eb
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-01-03 17:47:10 +0200

    avatar and ui

---
+++ src/pepse/util/UIConfiguration.java
@@ -0,0 +1,32 @@
+package pepse.util;
+
+import danogl.collisions.Layer;
+
+public class UIConfiguration {
+
+    public static int AVATAR_LAYER = Layer.UI;
+
+    public static String[] AVATAR_CONFIGS =
+            new String[]{"pepse\\util\\Birds\\tile000.png",
+                    "pepse\\util\\Birds\\tile001.png",
+                    "pepse\\util\\Birds\\tile002.png",
+                    "pepse\\util\\Birds\\tile003.png",
+                    "pepse\\util\\Birds\\tile004.png",
+                    "pepse\\util\\Birds\\tile005.png",
+                    "pepse\\util\\Birds\\tile006.png",
+
+                    "pepse\\util\\Birds\\tile008.png"
+            };
+//    public static String[] AVATAR_CONFIGS = new String[]{
+//            "pepse\\util\\AvatarImages\\Screenshot_20230102_084453.png",
+//            "pepse\\util\\AvatarImages\\Screenshot_20230102_084525.png",
+//            "pepse\\util\\AvatarImages\\Screenshot_20230102_084530.png",
+//            "pepse\\util\\AvatarImages\\Screenshot_20230102_084534.png",
+//            "pepse\\util\\AvatarImages\\Screenshot_20230102_084541.png",
+//            "pepse\\util\\AvatarImages\\Screenshot_20230102_084547.png",
+//            "pepse\\util\\AvatarImages\\Screenshot_20230102_084552.png"};
+
+
+
+
+}
