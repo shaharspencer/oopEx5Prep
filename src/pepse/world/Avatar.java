b76f8516a56f7fe64568dad77edd2f9549d6bb75
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-01-09 22:53:07 +0200

    for elli

--- src/pepse/world/Avatar.java
+++ src/pepse/world/Avatar.java
@@ -14,14 +14,17 @@ import pepse.util.configurations.TerrainConfiguration;
 import java.awt.event.KeyEvent;
 
 import java.io.File;
+import java.util.function.Function;
 
 public class Avatar extends GameObject {
     private static final String AVATAR_IMAGE_STANDING_FOLDER = "standing";
-    private static final double TIME_BETWEEN_CLIPS = 0.75;
+    private static final double TIME_BETWEEN_CLIPS = 1;
+    private static final float ENERGY_FACTOR = 0.5F;
+    private static GameObjectCollection gameObjects;
     private final AnimationRenderable standingAnimation;
     private static final String AVATAR_IMAGE_FOLDER_PATH = "./src/pepse/util/assets/retro_man/";
     private static final float VELOCITY_X = 250;
-    private static final float VELOCITY_Y = -700;
+    private static final float VELOCITY_Y = -400;
     private static final float GRAVITY = 500;
     private static final String AVATAR_IMAGE_RIGHT_FOLDER = "right";
     private static final String AVATAR_IMAGE_UP_FOLDER = "up";
@@ -29,13 +32,21 @@ public class Avatar extends GameObject {
     private final AnimationRenderable rightAnimation;
     private final AnimationRenderable upAnimation;
     private final AnimationRenderable leftAnimation;
+    private Mode mode;
     private UserInputListener inputListener;
+    private Function<Float, Float> terrainCallback;
+
+    private float energyLevel = 100;
+    private NumericLifeCounter energyCounter;
+
+    enum Mode{FLYING, NOT_FLYING};
 
 
     public Avatar(Vector2 pos, UserInputListener inputListener, ImageReader imageReader) {
 
         super(pos, Vector2.ONES.mult(50f), new AnimationRenderable(getAvatarConfigsStanding(),
                 imageReader, true, TIME_BETWEEN_CLIPS));
+
 
         this.inputListener = inputListener;
         this.rightAnimation = new AnimationRenderable(
@@ -50,11 +61,20 @@ public class Avatar extends GameObject {
         this.standingAnimation = new AnimationRenderable(getAvatarConfigsStanding(),
                 imageReader, true, TIME_BETWEEN_CLIPS);
         setPhysics();
+        this.mode = Mode.NOT_FLYING;
 
+
+    }
+
+    private void addEnergyCounter() {
+        this.energyCounter = new NumericLifeCounter(
+                Vector2.ZERO, new Vector2(30, 30), this.energyLevel);
+        gameObjects.addGameObject(energyCounter);
     }
 
     /**
      * returns a list of strings representing paths to avatar renderables for standing
+     *
      * @return String[] renderables
      */
     public static String[] getAvatarConfigsStanding() {
@@ -93,22 +113,27 @@ public class Avatar extends GameObject {
      * Returns:
      * A newly created representing the avatar.
      */
+
     public static Avatar create(GameObjectCollection gameObjects,
                                 int layer, Vector2 topLeftCorner,
                                 UserInputListener inputListener,
                                 ImageReader imageReader) {
+        Avatar.gameObjects = gameObjects;
         Vector2 avatarPlacement = AvatarConfiguration.initialAvatarLocation;
+
         return new Avatar(avatarPlacement, inputListener, imageReader);
+
 
     }
 
     /**
      * returns a list of strings representing paths to avatar renderables for going right
+     *
      * @return String[] renderables
      */
     public static String[] getAvatarConfigsRight() {
 
-        File dir = new File(AVATAR_IMAGE_FOLDER_PATH+AVATAR_IMAGE_RIGHT_FOLDER);
+        File dir = new File(AVATAR_IMAGE_FOLDER_PATH + AVATAR_IMAGE_RIGHT_FOLDER);
 
         File[] directoryListing = dir.listFiles();
 
@@ -124,6 +149,7 @@ public class Avatar extends GameObject {
 
     /**
      * returns a list of strings representing paths to avatar renderables for jumping
+     *
      * @return String[] renderables
      */
 
@@ -144,6 +170,7 @@ public class Avatar extends GameObject {
 
     /**
      * returns a list of strings representing paths to avatar renderables for going left
+     *
      * @return String[] renderables
      */
 
@@ -166,77 +193,155 @@ public class Avatar extends GameObject {
     /**
      * on collision with something make avatar velocity zero on some axis
      * so it does not dive into trees and blocks
-     * @param other The GameObject with which a collision occurred.
+     *
+     * @param other     The GameObject with which a collision occurred.
      * @param collision Information regarding this collision.
      *                  A reasonable elastic behavior can be achieved with:
      *                  setVelocity(getVelocity().flipped(collision.getNormal()));
      */
-        @Override
-        public void onCollisionEnter(GameObject other, Collision collision) {
+    @Override
+    public void onCollisionEnter(GameObject other, Collision collision) {
         super.onCollisionEnter(other, collision);
+        //todo: make sure the avatar doesnt collide with floor or trees.
+
+        float minYPlace = terrainCallback.apply(this.getCenter().x())
+                -
+                (float) this.getDimensions().y() /2;
+        if (this.getTopLeftCorner().y() >= minYPlace) {
+            this.mode = Mode.NOT_FLYING;
+        }
+
+        else{
+            this.mode = Mode.FLYING;
+        }
+
+
 
         // if we crashed into something lower than us, set y velocity to be 0
-        if (other.getCenter().y() > this.getCenter().y()){
-            setVelocity(new Vector2 (this.getVelocity().x(), 0));
+        if (other.getCenter().y() > this.getCenter().y()) {
+            setVelocity(new Vector2(this.getVelocity().x(), 0));
         }
         // if we definetly crashed into a block, set velocity = 0
-        if (other.getTag().equals(TerrainConfiguration.TOP_BLOCK_TAG)){
-            setVelocity(new Vector2 (0, 0));
+        if (other.getTag().equals(TerrainConfiguration.TOP_BLOCK_TAG)) {
+            setVelocity(new Vector2(0, 0));
 
         }
         // else set x velocity to be 0
 
-        else{
-            setVelocity(new Vector2 (0, this.getVelocity().y()));
+        else {
+            setVelocity(new Vector2(0, this.getVelocity().y()));
         }
 
-        }
+    }
 
     public void update(float deltaTime) {
         super.update(deltaTime);
 
+
         respondToPressedKey();
-        /*
-                // set the avatar's minimal y height at groundHeightAt(x) + half of the avatar's body
-        float minYPlace = terrain.groundHeightAt(this.getCenter().x()) -
-                (float) this.getDimensions().y() /2 + 0.01f;
-        if (this.getCenter().y() > minYPlace){
+        //todo: check if the section below with different numbers helps with sinking into the floor
+
+        // set the avatar's minimal y height at groundHeightAt(x) + half of the avatar's body
+        float minYPlace = terrainCallback.apply(this.getCenter().x())
+                -
+                (float) this.getDimensions().y() /2;
+        if (this.getTopLeftCorner().y() >= minYPlace) {
             this.setCenter(new Vector2(this.getCenter().x(), minYPlace));
+
         }
-         */
     }
 
     /**
      * responds to pressed key using an appropriate function
      */
     private void respondToPressedKey() {
+        turnRightLeftOrStraight();
 
-        if (inputListener.isKeyPressed(KeyEvent.VK_LEFT)) {
-            leftKeyIsPressed();
-        }
-
-        if (inputListener.isKeyPressed(KeyEvent.VK_RIGHT)) {
-            rightkeyIsPressed();
-        }
         if (inputListener.isKeyPressed(KeyEvent.VK_SPACE) &&
                 inputListener.isKeyPressed(KeyEvent.VK_DOWN)) {
 
             upAnddownArePressed();
-            return;
         }
         if (inputListener.isKeyPressed(KeyEvent.VK_SPACE) && getVelocity().y() == 0) {
             upKeyIsPressed();
-            return;
+        }
+        // TODO: WHEN AVATAR LANDS IT HAS NO ENERGY
+        else if (this.getVelocity() == Vector2.ZERO) {
+            noKeyIsPressedAndAvatarIsStanding();
         }
 
-        else if (this.getVelocity() == Vector2.ZERO){
-            noKeyIsPressedAndAvatarIsStanding();
+        if (this.mode.equals(Mode.NOT_FLYING)){
+            if (this.energyLevel < AvatarConfiguration.initialAvatarEnergyLevel){
+                this.energyLevel += ENERGY_FACTOR;
+            }
+        }
+
+        else{
+            this.energyLevel -= ENERGY_FACTOR;
+        }
+
+        flyingControllor();
+
+
+    }
+
+    private void turnRightLeftOrStraight() {
+        if (inputListener.isKeyPressed(KeyEvent.VK_LEFT)) {
+            leftKeyIsPressed();
+        }
+
+        else if (inputListener.isKeyPressed(KeyEvent.VK_RIGHT)) {
+            rightkeyIsPressed();
+        }
+
+        else{
+            this.renderer().setRenderable(this.upAnimation);
+        }
+    }
+
+    /**
+     * this functions controlls flting of avatar.
+     * if shift and space are pressed, calls fly()
+     * else sets to not flying mode
+     */
+
+    private void flyingControllor() {
+        if (inputListener.isKeyPressed(KeyEvent.VK_SHIFT) &&
+                inputListener.isKeyPressed(KeyEvent.VK_SPACE)){
+            fly();
+        }
+        else{
+            setNotFlying();
+        }
+    }
+
+    /**
+     * this function sets all appropriate fields to not flying mode
+     */
+    private void setNotFlying(){
+        this.mode = Mode.NOT_FLYING;
+        transform().setAccelerationY(GRAVITY);
+    }
+
+    /**
+     * try to start flying
+     * if we can't because energy level is zero, set to not flying mode
+     */
+    private void fly() {
+        if (this.energyLevel <=0){
+            setNotFlying();
+        }
+        else{
+            transform().setAccelerationY(0);
+            this.mode = Mode.FLYING;
+            this.renderer().setRenderable(this.upAnimation);
         }
     }
 
     private void noKeyIsPressedAndAvatarIsStanding() {
         this.renderer().setRenderable(standingAnimation);
         this.setDimensions((Vector2.ONES.mult(50f)));
+        this.physics().preventIntersectionsFromDirection(Vector2.ZERO);
     }
 
 
@@ -246,6 +351,8 @@ public class Avatar extends GameObject {
 
     private void upKeyIsPressed() {
         transform().setVelocityY(VELOCITY_Y);
+        transform().setVelocityX(0);
+        this.physics().preventIntersectionsFromDirection(Vector2.ZERO);
         this.renderer().setRenderable(upAnimation);
         this.setDimensions((Vector2.ONES.mult(50f)));
     }
@@ -269,6 +376,7 @@ public class Avatar extends GameObject {
         xVel += VELOCITY_X;
         transform().setVelocityX(xVel);
         this.renderer().setRenderable(rightAnimation);
+        this.physics().preventIntersectionsFromDirection(Vector2.ZERO);
         this.setDimensions((Vector2.ONES.mult(50f)));
     }
 
@@ -281,8 +389,22 @@ public class Avatar extends GameObject {
         xVel -= VELOCITY_X;
         transform().setVelocityX(xVel);
         this.renderer().setRenderable(leftAnimation);
+        this.physics().preventIntersectionsFromDirection(Vector2.ZERO);
         this.setDimensions((Vector2.ONES.mult(50f)));
     }
+
+    public void setTerrainCallback(Function<Float, Float> yCoordinateCallback) {
+        this.terrainCallback = yCoordinateCallback;
+    }
+
+
+    public float getEnergyLevel(){
+        return this.energyLevel;
+    }
+
+
+
+
 
 
 
